826e21b44ef72c2e4415bc6f1973decc9d3f1f1f
RonaldRoca718 <ronaldrock529@gmail.com>
2025-05-28 00:20:08 -0500

    Update sample.py

--- sample.py
+++ sample.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 @app.route('/user')
 def get_user():
-  user_id = request.args.get('id')
+  userid = request.args.get('id')
   conn = sqlite3.connect(':memory:')
   cursor = conn.cursor
-  cursor.execute("SELECT name FROM user WHERE id = {user_id}")
+  cursor.execute("SELECT name FROM user WHERE id = {userid}")
   
